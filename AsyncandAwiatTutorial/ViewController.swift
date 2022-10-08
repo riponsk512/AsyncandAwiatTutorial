c43202bd689c691421a5a2c3f7f4385c81f7b2fc
Ripon Sk <riponsk477@gmail.com>
2022-10-08 22:59:44 +0530

    initial commit

--- AsyncandAwiatTutorial/ViewController.swift
+++ AsyncandAwiatTutorial/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
-
+struct User:Codable{
+    var name:String?
+    var username:String?
+}
 class ViewController: UIViewController {
-
+var arrUser = [User]()
+    @IBOutlet weak var tables: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        Task.init {
+            let users = await fetchUserdata()
+            self.arrUser = users
+            tables.reloadData()
+        }
         // Do any additional setup after loading the view.
     }
 
 
 }
-
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrUser.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arrUser[indexPath.row].name
+        return cell
+    }
+    
+    
+}
+extension ViewController{
+    func fetchUserdata() async ->[User]{
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else
+        {
+            return []
+        }
+        do{
+            let(data,_) = try await URLSession.shared.data(from: url)
+            return try JSONDecoder().decode([User].self, from: data)
+            
+        }catch{
+            return []
+        }
+        
+    }
+}
